8481144c20742f0fd1923891042b55311f2b06a3
conqrappindia <conqrappindia@gmail.com>
2025-05-16 14:28:38 +0530

    demo

--- vars/stageDemo.groovy
+++ vars/stageDemo.groovy
@@ -2,22 +2,18 @@ def call(String WORKSPACE) {
      echo "###################################################\n CODE BUILD STARTED\n###################################################"
      echo "###################################################\n BUILDING MODULE\n###################################################"
      sh """
-     cd ${WORKSPACE}/modules
-     chmod +x ../gradlew
-     ../gradlew clean deploy
+     echo "${WORKSPACE}/modules"
+     
      """
      echo "###################################################\n MODULE BUILD COMPLETED\n###################################################"
      echo "###################################################\n BUILDING CLIENT-EXTENSIONS\n###################################################"
      sh """
-     cd ${WORKSPACE}/client-extensions
-     ../gradlew clean deploy
+     echo "${WORKSPACE}/client-extensions"
      """  
      echo "###################################################\n CLIENT-EXTENSIONS BUILD COMPLETED\n###################################################"
      echo "###################################################\n BUILDING THEMES\n###################################################"
      sh """
-     cd ${WORKSPACE}/themes/krypton-theme
-     npm install
-     node_modules/.bin/gulp deploy
+     echo "${WORKSPACE}/themes/krypton-theme"
      """
      echo "###################################################\n THEMES BUILD COMPLETED\n###################################################"
      echo "###################################################\n CODE BUILD COMPLETED\n###################################################"
